8feaf5f74f8e675e30d7ef2f730f98605d4847da
Alya <AlyaAlotibi@outlook.sa>
2021-06-26 06:30:45 +0300

    add DELETE question route

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -52,7 +52,7 @@ def create_app(test_config=None):
     })
 
   '''
-  @TODO: 
+  @TO1: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
@@ -83,13 +83,28 @@ def create_app(test_config=None):
       ,'currentCategory':category
     })
   '''
-  @TODO: 
+  @TO1: 
   Create an endpoint to DELETE question using a question ID. 
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    deletedQ=Question.query.filter(Question.id==question_id).one_or_none()
+    if deletedQ is None:
+      abort(404)
+    deletedQ.delete()
+    selection = Question.query.order_by(Question.id).all()
+    current_question = paginate_question(request, selection)
+    return jsonify({
+        'success': True,
+        'deleted': question_id,
+        'questions':current_question,
+        'totalQuestions': len(selection)
+      })
 
+    
   '''
   @TODO: 
   Create an endpoint to POST a new question, 
